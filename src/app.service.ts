0bbf83318b3b2c0c4fbbd8ae21f9d8bc1328478c
Edgard <eins.yeager@gmail.com>
2024-06-16 22:33:33 +0500

    added user info

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,6 +12,21 @@ export class AppService {
   }
   async getLeads({ query }): Promise<Array<any>> {
     const res: any = await this.client.request.get('/api/v4/leads', query);
-    return res.data._embedded.leads;
+    let result = res.data._embedded.leads;
+
+    for (let index = 0; index < result.length; index++) {
+      result[index].user = await this.getContacts(
+        result[index].responsible_user_id,
+      );
+    }
+
+    return result;
+  }
+  async getContacts(responsible_user_id) {
+    const contacts: any = await this.client.request.get('/api/v4/contacts', {
+      responsible_user_id: responsible_user_id,
+      limit: 1,
+    });
+    return contacts.data._embedded.contacts[0];
   }
 }
